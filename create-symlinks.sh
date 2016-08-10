81fbef8c2c9f1c26886e40c27e09bacc4c6eda5a
Jeremy Gold <jeremy.gold@ikegps.com>
2016-08-10 22:01:18 +1200

    Fixing path in create-symlinks.sh

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -7,7 +7,7 @@ do
 	if [ ! -e ~/$file ]
 	then
 		echo "~/$file does not exist - creating"
-		ln -s ~/MyDocs/$file ~/$file
+		ln -s ~/config-gold/$file ~/$file
     else
         echo "~/$file alredy exists - not overwriting"
 	fi
