6edd3288b968a94e24e657e2fdbe0a4765839049
Jeremy Gold <grobbledorf@gmail.com>
2018-10-04 22:28:32 +1300

    Improving git aliases and adding keychain for fish

--- config.fish
+++ config.fish
@@ -4,12 +4,22 @@ end
 
 alias tmux='tmux -2 a'
 alias gst='git status'
+alias gdv='git diff --vim'
+alias gf='git fetch'
 alias gfl='git fetch; and git l'
 alias gpoh='git push origin HEAD'
+alias gpfoh='git push --force origin HEAD'
 alias gco='git checkout'
+alias gr='git remote'
 alias ga='git add'
 alias gc='git commit'
+alias gl='git pull'
 alias ll='ls -lh'
 alias la='ls -lha'
 alias c='clear'
+alias logout='killall StartDwm'
+
+set hostname (hostname)
+/usr/bin/keychain $HOME/.ssh/id_rsa
+source $HOME/.keychain/$hostname-fish
 
