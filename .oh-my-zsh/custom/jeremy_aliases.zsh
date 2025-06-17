531e87a0f1b61dd6d7a65bdedb295a0a69d1ec66
Jeremy Gold <jeremy.gold@ikegps.com>
2025-06-17 20:23:41 +1200

    New aliases

--- .oh-my-zsh/custom/jeremy_aliases.zsh
+++ .oh-my-zsh/custom/jeremy_aliases.zsh
@@ -1,9 +1,17 @@
 alias avdev='aws-vault exec sso-ikegps-ai-dev_admin --'
 alias avprod='aws-vault exec sso-ikegps-ai-prod_admin --'
 
-alias gfl='git fetch && git l'
+alias gfl='git fetch --all && git l'
 alias gpoh='git push origin HEAD'
 alias gpfoh='git push --force origin HEAD'
 alias tmux='tmux -2 a'
 
+alias dcup='docker compose up'
+alias dcupb='docker compose up --build'
+alias dcd='docker compose down'
+alias dcdup='docker compose down && docker compose up'
+
+alias bastiprod='aws-vault exec sso-ikegps-koru-prod-developer -- basti connect --rds-cluster koru-postgis --local-port 5435'
+alias bastiprod-ro='aws-vault exec sso-ikegps-koru-prod-developer -- basti connect --rds-instance koru-postgis-instance-2 --local-port 5435'
+alias bastidev='aws-vault exec sso-ikegps-koru-dev-developer -- basti connect --rds-cluster koru-postgis --local-port 5433'
 
